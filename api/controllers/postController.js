7012b54d0c704b15d73ca5c935309e61ad7642ab
Olivier Nguyen <olivier_nguyen@live.fr>
2017-09-16 02:56:11 +0200

    Remove unused code

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -2,15 +2,6 @@ import mongoose from 'mongoose';
 
 const Post = mongoose.model('Post');
 
-exports.list_all_tasks = function (req, res) {
-    Task.find({}, function (err, task) {
-        if (err)
-            res.send(err);
-        res.json(task);
-    });
-};
-
-
 exports.createPost = (req, res) => {
     const newPost = new Post(req.body);
     newPost.save((err, task) => {
